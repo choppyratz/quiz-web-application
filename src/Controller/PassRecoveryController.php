4991b97193134d1fc23fbd4e067145186a7a7e41
German <GermanletchiK@yandex.ru>
2019-12-09 23:46:50 +0300

    'trying-to-repair-email'

--- src/Controller/PassRecoveryController.php
+++ src/Controller/PassRecoveryController.php
@@ -11,13 +11,22 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Symfony\Component\Mailer\Bridge\Google\Transport\GmailSmtpTransport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\RawMessage;
 
 class PassRecoveryController extends AbstractController
 {
     /**
      * @Route("/recovery", name="pass_recovery")
      */
-    public function index(Request $request): Response
+    public function index(Request $request, MailerInterface $mailer, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = new User();
         $form = $this->createForm(PassRecoveryType::class, $user);
@@ -26,40 +35,22 @@ class PassRecoveryController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted()) {
             $email = $user->getEmail(); 
-    
-            if ($this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $email])) {
-                $isInDB = "in";
+            $em = $this->getDoctrine()->getManager();
+            if (($handle = $em->getRepository(User::class)->findOneBy(['email' => $email]))) {
+                $password = $passwordEncoder->encodePassword($user, uniqid());
+                $handle->setPassword($password);
+                $em->persist($handle);
+                $em->flush();
+                
+                $message = new \Swift_Message('Test email');
+                $message->setFrom('nauyokas.bsuir@gmail.com');
+                $message->setTo('nauyokas.bsuir@gmail.com');
+                $message->setBody('test');
+
+                $mailer->send(new RawMessage('content'));
+                //$mailer->send($email);
             }
         }
-
-/*        $ans = "out";
-        $task = null;
-        if ($form->isSubmitted()) {
-            $task = $form->getData();
-
-            if ($this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $task])) {
-                $ans = "in";
-            }
-*/
-
-/*            $qb = $user->createQueryBuilder('p')
-            ->andWhere('p.price > :task')
-            ->setParameter('task', $task)
-            ->getQuery();
-            $em = $qb->execute();*/
-
-            //$em = $this->getDoctrine()->getManager();
-            //$email = $task->getEmail();
-
-            //$query = $em->createQuery("SELECT id FROM App\Entity\User  WHERE email = '{$task['email']}'");
-            //$users = $query->getResult(); 
-            //$repository->find();
-            //$response = new RedirectResponse('/');
-			//$response->prepare($request);
-		//
-			//return $response->send();
-            
-
 
         return $this->render('pass_recovery/index.html.twig', [
             'form' => $form->createView(),
